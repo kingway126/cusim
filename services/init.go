ee782b9d8ed383950c810df610bf56b82ccbb0c2
recardoz <1098977435@qq.com>
2020-08-26 11:24:01 +0800

    初始化用户

--- services/init.go
+++ services/init.go
@@ -5,16 +5,17 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	"github.com/recardoz/cusim/models"
+	"github.com/recardoz/cusim/utils"
+	"log"
+	"time"
 )
 
 //todo 數據庫配置信息
 const (
-	DB_HOST = "127.0.0.1"
-	DB_PORT = "3306"
-	DB_USER = "root"
-	//DB_PASS    = "Recardo-0126"
-	//DB_PASS	= "root"
-	DB_PASS    = "Gcbody-0865"
+	DB_HOST    = "127.0.0.1"
+	DB_PORT    = "3306"
+	DB_USER    = "root"
+	DB_PASS    = "recardo0126"
 	DB_NAME    = "cim"
 	DB_CHARSET = "utf8"
 	DB_DEBUG   = true
@@ -27,9 +28,10 @@ func init() {
 	var err error
 	db, err = gorm.Open("mysql", DB_USER+":"+DB_PASS+"@("+DB_HOST+":"+DB_PORT+")/"+DB_NAME+"?charset"+DB_CHARSET)
 	if err != nil {
+		log.Printf("[ERROR] 创建数据库连接失败: %s", err.Error())
 		panic(err.Error())
 	}
-	//db.LogMode(DB_DEBUG)
+	db.LogMode(DB_DEBUG)
 
 	db.Set("gorm:table_options", "ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;").AutoMigrate(&models.Users{})
 	db.Set("gorm:table_options", "ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;").AutoMigrate(&models.Chats{})
@@ -38,4 +40,19 @@ func init() {
 	db.Set("gorm:table_options", "ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;").AutoMigrate(&models.Apps{})
 
 	logs.Informational("连接数据库成功")
+
+	//判断是否存在数据库记录
+	user, count := make([]models.Users, 0), 0
+	if err := db.Find(&user).Count(&count).Error; err != nil {
+		panic("查询数据库失败！")
+	} else if count == 0 {
+		admin := new(models.Users)
+		admin.User = "admin"
+		admin.Pwd = utils.Sha1Pwd("123456")
+		admin.Uuid = utils.NewUuid()
+		admin.CreateAt = time.Now().Unix()
+		if err := db.Create(admin).Error; err != nil {
+			panic("创建初始用户失败")
+		}
+	}
 }
--- utils/email.go
+++ utils/email.go
@@ -1,32 +1,32 @@
 package utils
 
 import (
+	"fmt"
+	"github.com/astaxie/beego/logs"
 	"gopkg.in/gomail.v2"
 	"time"
-	"github.com/astaxie/beego/logs"
-	"fmt"
 )
+
 //控制发送email协程的数量
 type Mail struct {
-	Work 		int
-	Run 		int
-	Eve 		int
-	Live 		time.Duration
-	MailMsg		chan *gomail.Message
+	Work    int
+	Run     int
+	Eve     int
+	Live    time.Duration
+	MailMsg chan *gomail.Message
 }
 
 const (
-	HOST 	= "smtp.exmail.qq.com"
-	NAME 	= "recardo@supernovachina.com"
-	PORT 	= 465
+	HOST   = "smtp.exmail.qq.com"
+	NAME   = "recardo@supernovachina.com"
+	PORT   = 465
 	SECRET = "PEV9cg5vry5cNYsf"
 )
 
-
 var mailAuto *Mail
 
-//todo 初始化邮件发送功能
-func MailInit()  {
+// 初始化邮件发送功能
+func MailInit() {
 
 	//初始化Mail
 	mailAuto = new(Mail)
@@ -36,11 +36,13 @@ func MailInit()  {
 	mailAuto.Live = 30
 	mailAuto.MailMsg = make(chan *gomail.Message, 100)
 }
-//todo 打印工作情况
+
+// 打印工作情况
 func MailDump() {
 	fmt.Println("work:", mailAuto.Work, "||run:", mailAuto.Run)
 }
-//todo 新增任务
+
+// 新增任务
 func SendMsg(tit, msg string, email ...string) {
 	m := gomail.NewMessage()
 	m.SetHeader("From", NAME)
@@ -54,25 +56,28 @@ func SendMsg(tit, msg string, email ...string) {
 	//发送消息
 	mailAuto.MailMsg <- m
 }
-//todo 新增协程
+
+//新增协程
 func (c *Mail) AddRun() {
 	c.Run++
 	go c.sendMail()
 }
-//todo 控制协程数量
+
+// 控制协程数量
 func (c *Mail) Balance() {
-	need, num := c.Work / c.Eve, c.Work % c.Eve
+	need, num := c.Work/c.Eve, c.Work%c.Eve
 	if num > 0 {
 		need++
 	}
 	if need > c.Run {
 		//创建协程
-		for i := 0; i < need - c.Run; i++ {
+		for i := 0; i < need-c.Run; i++ {
 			c.AddRun()
 		}
 	}
 }
-// todo 发送邮件经常，30秒内自动退出
+
+// 发送邮件经常，30秒内自动退出
 func (c *Mail) sendMail() {
 	d := gomail.NewDialer(HOST, PORT, NAME, SECRET)
 
@@ -81,35 +86,35 @@ func (c *Mail) sendMail() {
 	open := false
 	for {
 		select {
-			case m, ok := <-c.MailMsg:
-				if !ok {
-					return
-				}
-				if !open {
-					if s, err = d.Dial(); err != nil {
-						logs.Error(err.Error())
-						return
-					}
-					open = true
-				}
-				if err := gomail.Send(s, m); err != nil {
-					logs.Error(err.Error())
-				}
-				//发送完消息，work减少1
-				c.Work--
-
-				// Close the connection to the SMTP server if no email was sent in
-				// the last 30 seconds.
-			case <-time.After(c.Live * time.Second):
-				if open {
-					if err := s.Close(); err != nil {
-						panic(err)
-					}
-					open = false
-				}
-				c.Run--
-				//退出协程
+		case m, ok := <-c.MailMsg:
+			if !ok {
 				return
 			}
+			if !open {
+				if s, err = d.Dial(); err != nil {
+					logs.Error(err.Error())
+					return
+				}
+				open = true
+			}
+			if err := gomail.Send(s, m); err != nil {
+				logs.Error(err.Error())
+			}
+			//发送完消息，work减少1
+			c.Work--
+
+			// Close the connection to the SMTP server if no email was sent in
+			// the last 30 seconds.
+		case <-time.After(c.Live * time.Second):
+			if open {
+				if err := s.Close(); err != nil {
+					panic(err)
+				}
+				open = false
+			}
+			c.Run--
+			//退出协程
+			return
+		}
 	}
 }
